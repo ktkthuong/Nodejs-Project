80f4dac3aaf629b79b5427a2a8c1c707802e541c
ktkthuong <ktkthuong@gmail.com>
2022-07-16 13:21:45 +0700

    setup basic

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const homeRoutes = require("./routes/home");
+const musterRoutes = require("./routes/muster");
+const otherInfoRoutes = require("./routes/other-info");
+const addStafffoRoutes = require("./routes/add-staff");
+const errorController = require("./controllers/error");
+
+const mongoose = require("mongoose");
+const Staff = require("./models/staff");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  Staff.findOne({ _id: "6290a7ed3f83e23b522bf9f0" })
+    .then((staff) => {
+      req.staff = staff;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use(homeRoutes);
+app.use(musterRoutes);
+app.use(otherInfoRoutes);
+app.use(addStafffoRoutes);
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    'mongodb+srv://ktkthuong:30062010phat@cluster0.gwa1tdk.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+  .then((result) => {
+    Staff.findOne()
+      .then((staff) => {
+        if (!staff) {
+          const staff = new Staff({
+            name: "Phạm Văn A",
+            doB: new Date(1995, 04, 06),
+            salaryScale: 1.5,
+            startDate: new Date(2021, 10, 25),
+            department: "Nhân sự",
+            annualLeave: 12,
+            image: "http://localhost:3005/",
+            workStatus: null,
+            workTimes: [],
+            totalTimesWork: null,
+            leaveInfoList: [],
+            bodyTemperature: [],
+            vaccineInfo: [],
+            infectCovidInfo: [],
+          });
+          return staff.save();
+        }
+      })
+      .then((a) => {
+        app.listen(3005);
+      });
+  })
+  .catch((err) => console.log(err));
